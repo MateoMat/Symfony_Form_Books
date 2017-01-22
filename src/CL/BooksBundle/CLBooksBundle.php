e8cda3166dcad4640c5f44fcdcc675cf3cd68ce6
MateoMat <m.maslank@interia.pl>
2017-01-22 15:01:48 +0100

    test

--- src/CL/BooksBundle/CLBooksBundle.php
+++ src/CL/BooksBundle/CLBooksBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace CL\BooksBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class CLBooksBundle extends Bundle
 {
 }
